374ae9473d44b6e507018f7254f030f993f848f7
Radu Costinas <r.costinas@gmx.es>
2022-11-07 18:01:34 +0100

    Update telescope settings

--- editors/nvim/after/plugin/keymap/telescope.lua
+++ editors/nvim/after/plugin/keymap/telescope.lua
@@ -2,6 +2,7 @@ local Remap = require('raducostinas.keymap')
 local nnoremap = Remap.nnoremap
 local nmap = Remap.nmap
 local builtin = require('telescope.builtin')
+local command_palette = require('telescope').extensions.command_palette
 
 nnoremap('<C-p>', ':Telescope')
 nnoremap('<leader><space>', function()
@@ -21,4 +22,7 @@ nnoremap('<leader>fb', function()
 end)
 nnoremap('<leader>fs', function()
     builtin.current_buffer_fuzzy_find()
+end)
+nnoremap('<leader>k', function()
+    command_palette.command_palette()
 end)
