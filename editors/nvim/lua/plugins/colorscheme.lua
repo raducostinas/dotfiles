b4d7de91ccae70daeff2e60360fd5b7b180c7fc0
Radu Costinas <r.costinas@gmx.es>
2024-02-13 21:51:00 +0100

    Switch nvim theme from gruvbox to tokionight

--- editors/nvim/lua/plugins/colorscheme.lua
+++ editors/nvim/lua/plugins/colorscheme.lua
@@ -1,43 +1,9 @@
 return {
- "ellisonleao/gruvbox.nvim",
- name = "gruvbox",
- lazy = false,
- priority = 1000,
- config = function()
-   require('gruvbox').setup({
-     usercurl = true,
-     underline = true,
-     bold = true,
-     italic = {
-       strings = true,
-       comments = true,
-       operators = false,
-       folds = true,
-     },
-     strikethrough = true,
-     invert_selection = false,
-     invert_signs = false,
-     invert_tabline = false,
-     invert_intend_guides = false,
-     inverse = true, -- invert background for search, diffs, statuslines and errors
-     contrast = "", -- can be "hard", "soft" or empty string
-     overrides = {},
-     dim_inactive = false,
-     transparent_mode = false,
-     palette_overrides = {
-       neutral_red = '#CC241D',
-       neutral_orange = '#CC241D',
-       bright_red = '#FB4934',
-       bright_orange = '#FB4934',
-       neutral_green = '#689D6A',
-       bright_green = '#8EC07C',
-       neutral_blue = '#30A2FF',
-       bright_blue = '#00C4FF',
-       neutral_purple = '#EA1179',
-       light_purple = '#F79BD3',
-     }
-   })
-
-   vim.cmd.colorscheme("gruvbox")
- end,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function ()
+    vim.cmd.colorscheme("tokyonight-night")
+  end
 }
