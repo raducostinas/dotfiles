b5fd6bd866145c81db5dee284080ac4bc48e1b2f
Radu Costinas <r.costinas@gmx.es>
2024-02-22 23:08:45 +0100

    Update oil file to show hidden file by default

--- editors/nvim/lua/plugins/oil.lua
+++ editors/nvim/lua/plugins/oil.lua
@@ -3,7 +3,7 @@ return {
   opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("oil").setup({})
+    require("oil").setup(require("config.oil"))
     require("config.keymap.oil").keymaps()
   end
 }
